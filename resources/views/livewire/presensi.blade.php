39b4336511d5ae31421e49e7ecb4e1deda697f66
Rizki Januar I. <rizki.20046@mhs.unesa.ac.id>
2025-05-22 18:23:55 +0700

    ✨ feat : setup livewire for as Halaman Presensi

---
+++ resources/views/livewire/presensi.blade.php
@@ -0,0 +1,4 @@
+<div>
+    {{-- Success is as dangerous as failure. --}}
+    <h1>Hello</h1>
+</div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Presensi;
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('presensi', Presensi::class)->name('presensi');
+});
 
 Route::get('/', function () {
     return view('welcome');
