883e83d248adc7d7c5737b5bbd7a0461a39365fa
Rizki Januar I. <rizki.20046@mhs.unesa.ac.id>
2025-07-13 09:37:23 +0700

    🐛 fix : fix a bug

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'locale' => env('APP_LOCALE', 'Asia/Jakarta'),
+    'locale' => env('APP_LOCALE', 'id'),
 
     'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
